f34200f5c03cac035ab4ac071b74c52ab4ee1db9
Julliana <bjulliana@gmx.com>
2018-04-01 19:52:13 -0400

    Header Color Corrected
    Correction of Header Color on Scroll

--- js/app.js
+++ js/app.js
@@ -15,20 +15,20 @@
 
   //Header add Class on Scroll & Back to Top
   window.addEventListener('scroll', function(){
-    header = document.querySelector('header');
     btButton = document.querySelector('.backtotop');
+    menuMobile = document.querySelector('.title-bar');
 
     offset = 500;
     scrollpos = window.scrollY;
 
 
     if (scrollpos > 400){
-      header.classList.add("small");
-      header.classList.remove("large");
+      menuMobile.classList.add("small");
+      menuMobile.classList.remove("large");
     }
     else {
-      header.classList.add("large");
-      header.classList.remove("small");
+      menuMobile.classList.add("large");
+      menuMobile.classList.remove("small");
     }
 
     if (scrollpos > offset) {
@@ -40,4 +40,39 @@
     }
   });
 
+  var aboutSection = document.querySelector('#about');
+  var gallerySection = document.querySelector('#gallery');
+  var anniversarySection = document.querySelector('#anniversary');
+  var contactSection = document.querySelector('#contact');
+  var castSection = document.querySelector('#cast');
+  var header = document.querySelector('header');
+  var menu = document.querySelector('.lateralMenu');
+
+
+  //Change Menu Color
+  function changeHeaderColor() {
+    let aboutOffset = aboutSection.getBoundingClientRect();
+    let galleryOffset = gallerySection.getBoundingClientRect();
+    let anniversaryOffset = anniversarySection.getBoundingClientRect();
+    let contactOffset = contactSection.getBoundingClientRect();
+
+    if(aboutOffset.top < (menu.offsetHeight) && aboutOffset.bottom > (menu.offsetHeight/2)) {
+      header.classList.add('dark-menu');
+    }
+    else if(galleryOffset.top < (menu.offsetHeight) && galleryOffset.bottom > (menu.offsetHeight/2)) {
+      header.classList.add('dark-menu');
+    }
+    else if(anniversaryOffset.top < (menu.offsetHeight) && anniversaryOffset.bottom > (menu.offsetHeight/2)) {
+      header.classList.add('dark-menu');
+    }
+    else if(contactOffset.top < (menu.offsetHeight) && contactOffset.bottom > (menu.offsetHeight/2)) {
+      header.classList.add('dark-menu');
+    }
+    else {
+      header.classList.remove('dark-menu');
+    }
+  }
+
+
+  window.addEventListener('scroll', changeHeaderColor);
 })();
